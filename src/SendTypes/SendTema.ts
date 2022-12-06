6bee12f13b25e827ab24928fb213a56e13481e16
gonzalo errandonea <errandoneagonzalo18@gmail.com>
2022-12-05 22:30:09 -0300

    tipo SendTema implementado

---
+++ src/SendTypes/SendTema.ts
@@ -0,0 +1,15 @@
+import { Field, Int, ObjectType } from "type-graphql";
+import { Tema } from "../Entities/Tema";
+
+@ObjectType()
+export class SendTema
+{
+    @Field()
+    message: string = "";
+
+    @Field()
+    success:boolean = false;
+
+    @Field(type => Tema, {nullable: true})
+    temas?: Tema[]
+}
